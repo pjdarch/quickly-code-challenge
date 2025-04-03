e293ebf42e04f9487099c4225a953ec4fa08003d
Phillip Darch <pjdarch@gmail.com>
2025-04-02 20:54:09 -0600

    Add shadcn login form components, apply styling, and assets

---
+++ components/login-form.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Log in</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+        Welcome back! Please sign in to continue.
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="Business email" required />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </div>
+    </form>
+  )
+}
