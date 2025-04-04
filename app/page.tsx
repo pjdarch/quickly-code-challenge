25e55462e9533185d1724645c1b34f3aa2ab91fd
Phillip Darch <pjdarch@gmail.com>
2025-04-03 22:25:21 -0600

    Fix alignment and display of logo

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
+        <div className="flex justify-left gap-2 md:justify-start">
           <Image
             src="/logo.svg"
             width={108}
             height={32}
             alt="Logo"
-            className="hidden h-8 w-auto lg:block" />
+            className="h-8 w-auto block lg:block" />
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
